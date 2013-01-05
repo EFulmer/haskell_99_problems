511d407d30c4fd73ffbca9bd0b28fc3648a949ef
EFulmer <EFulmer@gmail.com>
2013-01-05 17:53:41 -0500

    problem seven

--- problem_seven.hs
+++ problem_seven.hs
@@ -1,16 +1,34 @@
 -- Problem 7:
 -- Flatten a nested list structure.
 
+import Data.List
+
+-- going to do this for both standard nested lists (type [[a]]) and the
+-- NestedList type
+
+data NestedList a = Elem a | List [NestedList a]
+
+nestedFlatten :: NestedList a -> [a]
+nestedFlatten (Elem x)      = [x]
+nestedFlatten (List [])     = []
+nestedFlatten (List (x:xs)) = nestedFlatten x  ++ (nestedFlatten $ List xs)
+
+-- by folding:
+nestedFlatten' :: NestedList a -> [a]
+nestedFlatten' (Elem x)  = [x]
+nestedFlatten' (List []) = []
+nestedFlatten' (List xs) = foldl1' (++) $ map nestedFlatten' xs
+
 -- concat in Data.List is a Haskell-provided version, but that's no fun!
 
--- fold to the left
+-- left fold:
 flatten :: [[a]] -> [a]
 flatten xs = foldl (\acc x -> acc ++ x) [] xs
 
-flatten' :: [[a]] -> [a]
-flatten' xs = foldr (\x acc -> 
+-- flatten' :: [[a]] -< [a]
+-- flatten' xs = 
 
-testOne = ["Raisans", "Sludgefeast", "Tarpit"]
+testOne = ["Raisans, ", "Sludgefeast, ", "Tarpit, "]
 testTwo = [[100], [200], [300]]
 
 main :: IO ()
