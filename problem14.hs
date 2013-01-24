fc1ebea61ca0169417d3fcb2f13fc04ee6fb41fc
EFulmer <EFulmer@gmail.com>
2013-01-23 20:13:20 -0500

    even more reorganizing

--- problem14.hs
+++
@@ -1,6 +0,0 @@
--- Problem 14:
--- Duplicate the elements of a list.
-
-duplicate :: [a] -> [a]
-duplicate []     = []
-duplicate (x:xs) = x:x:(duplicate xs)
--- problemThirtyTwo.hs
+++
@@ -1,8 +0,0 @@
--- Problem 32:
--- Determine the greatest common divisor of two positive integer numbers. 
--- Use Euclid's algorithm.
-
-euclid :: Integral a => a -> a -> a
-euclid x y 
-       | y == 0    = x
-       | otherwise = euclid y (x `mod` y)
