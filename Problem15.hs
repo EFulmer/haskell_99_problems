451fc75107826f718f5e168b9f9c354066b9484b
EFulmer <EFulmer@gmail.com>
2013-01-24 12:18:15 -0500

    15

--- Problem15.hs
+++ Problem15.hs
@@ -1,6 +1,34 @@
 -- Problem 15:
 -- Replicate the elements of a list a given number of times.
 
-repli :: [a] -> Int -> [a]
-repli [] _     = []
-repli (x:xs) n = (take n $ repeat x) ++ (repli xs n)
+
+-- First approach: just use Prelude's replicate function. Uninteresting.
+repli :: [a] -> [a]
+repli []     = []
+repli (x:xs) = (replicate 2 x) ++ (repli xs)
+
+-- Second approach: use take-repeat to mimic replicate. Still boring.
+repli' :: [a] -> [a]
+repli' []     = []
+repli' (x:xs) = (take 2 $ repeat x) ++ (repli xs)
+
+-- Now with monads. Now we're talking.
+repli'' :: [a] -> [a]
+repli'' xs = xs >>= (\x -> [x,x])
+
+testOne = [1,2,3]
+testTwo = [Just "mayonaise", Nothing]
+
+-- Should print
+-- [1,1,2,2,3,3]
+-- [Just "mayonaise",Just "mayonaise",Nothing,Nothing]
+-- three times in a row
+
+main :: IO ()
+main = do
+     putStrLn . show $ repli testOne
+     putStrLn . show $ repli testTwo
+     putStrLn . show $ repli' testOne
+     putStrLn . show $ repli' testTwo
+     putStrLn . show $ repli'' testOne
+     putStrLn . show $ repli'' testTwo
