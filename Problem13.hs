7ef923700f57788693f56a0095154f0c72c28355
EFulmer <EFulmer@gmail.com>
2013-06-30 21:29:44 -0400

    problem 13 (finally!)

--- Problem13.hs
+++ Problem13.hs
@@ -5,8 +5,20 @@
 -- duplicates, as in problem 9, but only count them. As in problem P11, 
 -- simplify the result list by replacing the singleton lists (1 X) by X.
 
+module Problem14 (encodeDirect) where
+
 data ListItem a = Single a | Multiple Int a deriving (Eq, Ord, Show)
+
+acc :: Eq a => Int -> [a] -> [ListItem a]
+acc n (x:[]) = if n > 1 
+               then [Multiple n x]
+               else [Single  x]
+acc n xs@(x:y:ys) = if x == y 
+                    then acc (n + 1) (y:ys)
+                    else if n > 1 
+                         then (Multiple n x):(acc 1 (y:ys))
+                         else (Single x):(acc 1 (y:ys))
 
 encodeDirect :: Eq a => [a] -> [ListItem a]
 encodeDirect [] = []
-encodeDirect (x:xs) = 
+encodeDirect xs = acc 1 xs
